3330423722ab9000bc8e16022c3cfdeadfbf4645
Jaqueline Romero <csi23-jromres@colegioaltair.net>
2023-10-01 20:23:19 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,38 @@
+﻿using EcobiciApp.Servicios;
+
+namespace EcobiciApp.Controladores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuInterfaz mi = new MenuImplementacion();
+            mi.mensajeHola();
+
+            bool cerrarMenu = false;
+
+            while (!cerrarMenu)
+            {
+                int numero = mi.opcionesMenu();
+                switch(numero)
+                {
+                    case 0:
+                        Console.WriteLine("Vuelve pronto");
+                        cerrarMenu = true;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Aqui te mostramos las tarifas:");
+                        Console.WriteLine("Un día 2 eur");
+                        Console.WriteLine("Un mes 23 eur");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("libera una bici");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcobiciApp.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public void mensajeHola()
+        {
+            Console.WriteLine("Bienvenido a Ecobici");
+        }
+
+        public int opcionesMenu()
+        {
+            int numero; 
+
+            Console.WriteLine("Estás en la pagina principal de Ecobici. Seleccione la opcion para comenzar");
+            Console.WriteLine("0. Cerrar aplicación");
+            Console.WriteLine("1. Ver tarifas");
+            Console.WriteLine("2. Liberar una bici");
+           
+            numero = Console.ReadKey(true).KeyChar-('0');
+            
+            return numero;
+        }
+    }
+}
